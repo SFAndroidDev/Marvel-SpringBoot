ce0101d4d3b8d85f3da6bf95a98bac0b222c717e
SF <sfnaiech2016@gmail.com>
2021-01-09 10:51:33 +0100

    Spring boot project created

---
+++ src/main/java/com/sfdevs/marvel/MarvelSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.sfdevs.marvel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sfdevs/marvel/MarvelSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfdevs.marvel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
