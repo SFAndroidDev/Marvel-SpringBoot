4819a5f2010674b7e85883c9f66874218770078e
SF <sfnaiech2016@gmail.com>
2021-01-09 12:06:42 +0100

    find user by id method added to controller

--- src/main/java/com/sfdevs/marvel/controller/UserController.java
+++ src/main/java/com/sfdevs/marvel/controller/UserController.java
@@ -3,7 +3,10 @@ package com.sfdevs.marvel.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +25,16 @@ public class UserController {
 	@GetMapping("users")
 	public List<User> getAllUser(){
 		return this.userRepository.findAll();
+	}
+	
+	// find a user by his Id
+	@GetMapping("users/{id}")
+	public ResponseEntity<User> getUserById(@PathVariable(value = "id") long userId )
+	throws ResourceNotFoundException {
+		User user = userRepository.findById(userId).orElseThrow(() ->
+				new ResourceNotFoundException("User not found for the id :: " 
+						+userId));
+		return ResponseEntity.ok().body(user);
 	}
 	
 }
